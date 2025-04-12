1d564aad23480226e5605c261d23fc742561e908
Game data <160002063+temcs@users.noreply.github.com>
2025-04-12 20:23:06 +0800

    Add files via upload

---
+++ 8mdata.js
@@ -0,0 +1,387 @@
+// Define sets of animal names for each time frame and day
+const gameset = {
+
+sunday: {
+"9am-3pm": [
+"76% - KA স্লটের Bonus Mania",
+"81% - BGM ক্রাশের Minesweeper",
+"80% - PEGASUS স্লটের Toy Soldiers",
+"84% - SPB ক্রাশের Aviator",
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"79% - BGM স্লটের Beast Band",
+"81% - JOKER স্লটের Chilli Hunter",
+"80% - VPOWER স্লটের Cookie Pop",
+"83% - LGD স্লটের Field Of Honor"
+],
+"3pm-9pm": [
+"80% - NAGA স্লটের Dragon Wonder",
+"79% - BGM স্লটের Fortuna Trueways",
+"88% - SPB ক্রাশের Aviator",
+"80% - VPOWER স্লটের Captain Treasure",
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"80% - PEGASUS স্লটের Tianhou Mazu",
+"83% - JDB স্লটের Go Lai Fu",
+"79% - BGM স্লটের Beast Band",
+"79% - BNG স্লটের wolf saga",
+"83% - CQ9 স্লটের Flying Cai Shen"
+],
+"9pm-3am": [
+"75% - HOTDOG স্লটের Lady Hades",
+"90% - SPB ক্রাশের Aviator",
+"80% - RG স্লটের Madame Gypsy",
+"80% - RICH88 স্লটের Fortune Dragon",
+"79% - FACHAI স্লটের Gold Rush",
+"80% - PEGASUS স্লটের Toy Soldiers",
+"76% - PLAYSTAR স্লটের Aurora Wolf",
+"81% - SIMPLE স্লটের Fantasy Goddess",
+"79% - FACHAI স্লটের Gold Rush",
+"75% - EVO ক্যাসিনোর Dream Catcher"
+],
+"3am-9am": [
+"79% - KM স্লটের Gold Rush Bonanza",
+"75% - NEXTSPIN স্লটের Golden West",
+"83% - LGD স্লটের Eye Of Egypt",
+"79% - FACHAI স্লটের Tower",
+"76% - KA স্লটের Bonus Mania",
+"86% - SPB ক্রাশের Aviator",
+"83% - JDB স্লটের Go Lai Fu",
+"79% - FACHAI স্লটের Gold Rush",
+"79% - BNG স্লটের wolf saga",
+"81% - JILI স্লটের Crazy Hunter 2"
+]
+},
+monday: {
+"9am-3pm": [
+"81% - JOKER স্লটের Genie Huga",
+"79% - PP স্লটের Zeus vs Hades - Gods of War™",
+"79% - BNG স্লটের wolf saga",
+"85% - SPB ক্রাশের Aviator",
+"76% - PLAYSTAR স্লটের Kung Hei",
+"82% - pp ক্যাসিনোর Sweet Bonanza CandyLand",
+"82% - pp ক্যাসিনোর Snakes & Ladders Live",
+"80% - RG স্লটের Vegas Night",
+"83% - CQ9 স্লটের Flying Cai Shen",
+"75% - HOTDOG স্লটের Lady Hades"
+],
+"3pm-9pm": [
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"80% - RICH88 স্লটের Fortune Dragon",
+"79% - PP স্লটের Candy Jar Cluster™",
+"83% - SPB ক্রাশের Aviator",
+"79% - KM স্লটের London Mystery",
+"83% - JDB স্লটের Fortune Neko",
+"79% - BNG স্লটের wukong",
+"79% - HBN স্লটের Gangsters",
+"79% - BGM স্লটের Big Atlantis Frenzy",
+"83% - CQ9 স্লটের Jump Higher mobile"
+],
+"9pm-3am": [
+"79% - KM স্লটের Gold Rush Bonanza",
+"76% - KA স্লটের American Burger",
+"79% - KM স্লটের Gold Rush Bonanza",
+"80% - NAGA স্লটের Gladiators",
+"79% - KM স্লটের London Mystery",
+"83% - SPB ক্রাশের Aviator",
+"79% - BGM স্লটের Beast Band",
+"79% - FASTSPIN স্লটের Fiery Lava",
+"79% - FASTSPIN স্লটের Adventure To The West"
+],
+"3am-9am": [
+"83% - CQ9 স্লটের Flying Cai Shen",
+"76% - PLAYSTAR স্লটের Dragon Boat Festival",
+"82% - PG স্লটের Wild Bandito",
+"79% - HBN স্লটের Gangsters",
+"76% - KA স্লটের American Burger",
+"84% - SPB ক্রাশের Aviator",
+"76% - SG স্লটের 888",
+"79% - BGM স্লটের Beast Band",
+"80% - NAGA স্লটের Gladiators"
+]
+},
+tuesday: {
+"9am-3pm": [
+"75% - EVO ক্যাসিনোর Funky Time",
+"87% - SPB ক্রাশের Aviator",
+"79% - FASTSPIN স্লটের Adventure To The West",
+"76% - SG স্লটের Gold Panther Maxways",
+"80% - PEGASUS স্লটের Toy Soldiers",
+"81% - BGM ক্রাশের PlinkoXY",
+"81% - SIMPLE স্লটের Bomb Adventure",
+"86% - JILI স্লটের Mega Ace",
+"81% - HAK ক্রাশের Speed Crash",
+"79% - FASTSPIN স্লটের The Great Safari"
+],
+"3pm-9pm": [
+"75% - HOTDOG স্লটের Lady Hades",
+"86% - SPB ক্রাশের Aviator",
+"74% - REEVO স্লটের Electro Goals",
+"75% - EVO ক্যাসিনোর Mega Ball",
+"79% - PP স্লটের Forge of Olympus™",
+"79% - FACHAI স্লটের Hot Pot Party",
+"79% - BNG স্লটের hit the gold",
+"82% - PG স্লটের Wild Bounty Showdown",
+"80% - VPOWER স্লটের Cookie Pop",
+"83% - JDB স্লটের Trump Card"
+],
+"9pm-3am": [
+"76% - PLAYSON স্লটের Royal Coins2: Hold and Win",
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"74% - REEVO স্লটের Chubby Santa",
+"91% - SPB ক্রাশের Aviator",
+"79% - HBN স্লটের Gangsters",
+"83% - JDB স্লটের Trump Card",
+"80% - NAGA স্লটের Dragon Wonder",
+"80% - PEGASUS স্লটের Tianhou Mazu",
+"80% - NAGA স্লটের Gladiators",
+"80% - PEGASUS স্লটের Crazy Thieves"
+],
+"3am-9am": [
+"75% - HOTDOG স্লটের Lady Hades",
+"80% - RG স্লটের Madame Gypsy",
+"80% - RICH88 স্লটের Fortune Dragon",
+"79% - FACHAI স্লটের Gold Rush",
+"87% - SPB ক্রাশের Aviator",
+"80% - PEGASUS স্লটের Toy Soldiers",
+"76% - PLAYSTAR স্লটের Aurora Wolf",
+"81% - SIMPLE স্লটের Fantasy Goddess",
+"87% - JILI স্লটের Mega Ace",
+"75% - EVO ক্যাসিনোর Dream Catcher"
+]
+},
+wednesday: {
+"9am-3pm": [
+"82% - pp ক্যাসিনোর Sweet Bonanza CandyLand",
+"75% - EVO ক্যাসিনোর Mega Ball",
+"75% - EVO ক্যাসিনোর Dream Catcher",
+"83% - SPB ক্রাশের Aviator",
+"79% - BGM স্লটের Big Atlantis Frenzy",
+"80% - RICH88 স্লটের King Kong",
+"83% - JDB স্লটের Fortune Neko",
+"81% - JILI স্লটের Mega Ace",
+"79% - FACHAI স্লটের Tower"
+],
+"3pm-9pm": [
+"83% - LGD স্লটের Clash of Three kingdoms",
+"83% - JDB স্লটের Fortune Neko",
+"79% - HBN স্লটের Gangsters",
+"74% - REEVO স্লটের Electro Goals",
+"92% - SPB ক্রাশের Aviator",
+"76% - KA স্লটের American Burger",
+"79% - BNG স্লটের green chilli 2",
+"79% - BGM স্লটের Big Atlantis Frenzy",
+"80% - RG স্লটের Madame Gypsy",
+"75% - EVO ক্যাসিনোর Mega Ball"
+],
+"9pm-3am": [
+"80% - RG স্লটের Vegas Night",
+"91% - SPB ক্রাশের Aviator",
+"75% - EVO ক্যাসিনোর Funky Time",
+"81% - BGM ক্রাশের PlinkoXY",
+"75% - NEXTSPIN স্লটের Soccer King",
+"83% - JDB স্লটের Go Lai Fu",
+"79% - PP স্লটের Candy Jar Cluster™",
+"83% - LGD স্লটের Field Of Honor",
+"81% - JILI স্লটের Fortune Gems 2 ",
+"81% - SIMPLE স্লটের Fantasy Goddess"
+],
+"3am-9am": [
+"81% - JILI স্লটের Fortune Gems 2 ",
+"80% - PEGASUS স্লটের Toy Soldiers",
+"76% - SG স্লটের Double Flame",
+"85% - JILI স্লটের Mega Ace",
+"90% - SPB ক্রাশের Aviator",
+"82% - pp ক্যাসিনোর Sweet Bonanza CandyLand",
+"76% - SG স্লটের Gold Panther Maxways",
+"79% - FASTSPIN স্লটের The Great Safari",
+"83% - JDB স্লটের Fortune Neko",
+"79% - PP স্লটের Candy Jar Cluster™"
+]
+},
+thursday: {
+"9am-3pm": [
+"81% - JILI স্লটের Fortune Gems 2 ",
+"90% - SPB ক্রাশের Aviator",
+"82% - pp ক্যাসিনোর Mega Wheel",
+"74% - REEVO স্লটের Electro Goals",
+"87% - JILI স্লটের Mega Ace",
+"80% - NAGA স্লটের Pumpkin Night",
+"79% - KM স্লটের London Mystery",
+"75% - HOTDOG স্লটের Break the Ice!",
+"83% - JDB স্লটের Fruity Bonanza",
+"76% - PLAYSON স্লটের Royal Coins2: Hold and Win"
+],
+"3pm-9pm": [
+"81% - HAK ক্রাশের Speed Crash",
+"81% - BGM ক্রাশের PlinkoXY",
+"76% - KA স্লটের American Burger",
+"76% - SG স্লটের Double Flame",
+"79% - HBN স্লটের Gangsters",
+"84% - JILI স্লটের Mega Ace",
+"79% - KM স্লটের Gold Rush Bonanza",
+"83% - SPB ক্রাশের Aviator",
+"79% - HBN স্লটের Galactic Cash"
+],
+"9pm-3am": [
+"82% - PG স্লটের Wild Bandito",
+"79% - BNG স্লটের hit the gold",
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"81% - JOKER স্লটের Black Beard Legacy",
+"81% - HAK ক্রাশের Speed Crash",
+"85% - SPB ক্রাশের Aviator",
+"79% - HBN স্লটের Bikini Island",
+"82% - pp ক্যাসিনোর Snakes & Ladders Live",
+"79% - KM স্লটের Gold Rush Bonanza"
+],
+"3am-9am": [
+"81% - SIMPLE স্লটের Fantasy Goddess",
+"90% - SPB ক্রাশের Aviator",
+"79% - FASTSPIN স্লটের Adventure To The West",
+"81% - JOKER স্লটের Black Beard Legacy",
+"79% - BNG স্লটের wukong",
+"82% - PG স্লটের Wild Bandito",
+"86% - JILI স্লটের Mega Ace",
+"79% - BGM স্লটের Forgotten",
+"75% - NEXTSPIN স্লটের Holy Goat",
+"83% - LGD স্লটের Clash of Three kingdoms"
+]
+},
+friday: {
+"9am-3pm": [
+"79% - BGM স্লটের Beast Band",
+"80% - RG স্লটের Crystal Veins",
+"80% - VPOWER স্লটের Easter Surprise",
+"79% - BNG স্লটের wukong",
+"75% - EVO ক্যাসিনোর Funky Time",
+"86% - SPB ক্রাশের Aviator",
+"76% - SG স্লটের Double Flame",
+"81% - JILI স্লটের Fortune Gems 2 ",
+"80% - RICH88 স্লটের King Kong",
+"83% - JDB স্লটের Fortune Neko"
+],
+"3pm-9pm": [
+"79% - BNG স্লটের green chilli 2",
+"81% - BGM ক্রাশের PlinkoXY",
+"79% - PP স্লটের Forge of Olympus™",
+"76% - PLAYSTAR স্লটের Aurora Wolf",
+"81% - JOKER স্লটের Chilli Hunter",
+"80% - PEGASUS স্লটের Tianhou Mazu",
+"90% - SPB ক্রাশের Aviator",
+"79% - KM স্লটের Gold Rush Bonanza",
+"76% - PLAYSON স্লটের Royal Coins Hold and Win",
+"74% - REEVO স্লটের Electro Goals"
+],
+"9pm-3am": [
+"83% - JDB স্লটের Fruity Bonanza",
+"83% - CQ9 স্লটের Flying Cai Shen",
+"83% - JDB স্লটের Fortune Neko",
+"79% - HBN স্লটের Dragon's Realm",
+"81% - JOKER স্লটের Chilli Hunter",
+"81% - HAK ক্রাশের Speed Crash",
+"83% - SPB ক্রাশের Aviator",
+"75% - EVO ক্যাসিনোর Dream Catcher",
+"81% - JILI স্লটের Fortune Gems 2 "
+],
+"3am-9am": [
+"75% - HOTDOG স্লটের Black Myth: Wukong",
+"79% - KM স্লটের Gold Rush Bonanza",
+"75% - EVO ক্যাসিনোর Mega Ball",
+"79% - FACHAI স্লটের Gold Rush",
+"81% - JILI স্লটের Mega Ace",
+"86% - SPB ক্রাশের Aviator",
+"79% - FASTSPIN স্লটের The Great Safari",
+"83% - CQ9 স্লটের Flying Cai Shen",
+"81% - JOKER স্লটের Genie Huga",
+"76% - PLAYSTAR স্লটের Dragon Boat Festival"
+]
+},
+saturday: {
+"9am-3pm": [
+"79% - BGM স্লটের Forgotten",
+"79% - HBN স্লটের Bikini Island",
+"75% - NEXTSPIN স্লটের Golden West",
+"91% - SPB ক্রাশের Aviator",
+"76% - SG স্লটের Double Flame",
+"75% - NEXTSPIN স্লটের Soccer King",
+"76% - PLAYSON স্লটের Luxor Gold Hold and Win",
+"82% - pp ক্যাসিনোর Fortune Roulette",
+"79% - KM স্লটের London Mystery",
+"82% - pp ক্যাসিনোর Sweet Bonanza CandyLand"
+],
+"3pm-9pm": [
+"83% - LGD স্লটের Field Of Honor",
+"80% - RG স্লটের Madame Gypsy",
+"80% - PEGASUS স্লটের Crazy Thieves",
+"75% - EVO ক্যাসিনোর Mega Ball",
+"89% - SPB ক্রাশের Aviator",
+"75% - HOTDOG স্লটের Lady Hades",
+"80% - VPOWER স্লটের Cookie Pop",
+"75% - NEXTSPIN স্লটের Holy Goat",
+"87% - JILI স্লটের Mega Ace",
+"81% - JOKER স্লটের Genie Huga"
+],
+"9pm-3am": [
+"81% - JILI স্লটের Mega Ace",
+"76% - SG স্লটের 888",
+"81% - HAK ক্রাশের Speed Crash",
+"80% - RG স্লটের Madame Gypsy",
+"83% - JDB স্লটের Go Lai Fu",
+"90% - SPB ক্রাশের Aviator",
+"81% - BGM ক্রাশের Heads and Tails",
+"79% - FASTSPIN স্লটের City of Jewels",
+"83% - CQ9 স্লটের Flying Cai Shen"
+],
+"3am-9am": [
+"76% - SG স্লটের Gold Panther Maxways",
+"82% - pp ক্যাসিনোর Mega Wheel",
+"81% - SIMPLE স্লটের Fantasy Goddess",
+"79% - HBN স্লটের Bikini Island",
+"86% - SPB ক্রাশের Aviator",
+"76% - PLAYSON স্লটের Royal Coins2: Hold and Win",
+"80% - RG স্লটের Crystal Veins",
+"83% - JDB স্লটের Trump Card",
+"79% - FASTSPIN স্লটের The Great Safari",
+"75% - NEXTSPIN স্লটের Forest Secret"
+]
+},
+  
+  
+      
+    };
+    
+    
+    // Function to get the current day and time
+    function getCurrentTime() {
+      const now = new Date();
+      const day = now.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+      const hour = now.getHours();
+    
+      return {
+        day,
+        hour
+      };
+    }
+    
+  // Function to display game names based on current time
+  function displayGameNames() {
+      const currentTime = getCurrentTime();
+      const dayOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'][currentTime.day];
+      const timeOfDay =
+        currentTime.hour >= 9 && currentTime.hour < 15 ? "9am-3pm" :
+        currentTime.hour >= 15 && currentTime.hour < 21 ? "3pm-9pm" :
+        currentTime.hour >= 21 || currentTime.hour < 3 ? "9pm-3am" :
+        "3am-9am";
+    
+      const gameNames = gameset[dayOfWeek.toLowerCase()][timeOfDay];
+    
+      const gameNamesContainer = document.getElementById("gameList");
+      // Clear previous content
+      gameNamesContainer.innerHTML = "";
+      // Append new game names
+      gameNames.forEach(game => {
+        gameNamesContainer.innerHTML += `<span>✔️ উত্তোলন - ${game}</span><br>`;
+      });
+    }
+    
+    // Call the function initially and every 10 seconds
+    displayGameNames();
+    setInterval(displayGameNames, 10000);
